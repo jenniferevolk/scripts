6169f2b8d50f9323124cdf180c78e99b1c94ee11
Jennifer E Volk <jenniferevolk@gmail.com>
2017-03-31 19:26:27 -0700

    a lot of refactoring and debugging. fixed heroku installer, added whois to apps, improved logging etc

--- setup.sh
+++ setup.sh
@@ -14,54 +14,66 @@ NC='\033[0m' # No Color
 # app lists
 system="thermald microcode.ctl intel-microcode preload"
 
-clitools="powertop htop libcurl3 libav-tools ffmpeg xclip tmux"
+clitools="powertop htop whois libcurl3 libav-tools ffmpeg xclip tmux tilda traceroute nmap lame soundstretch libid3-3.8.3v5"
 
 compression="p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract"
 
-codecs="chromium-codecs-ffmpeg-extra gstreamer1.0-libav gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libavcodec-extra libdvdcss2 libdvdnav4 libdvdread4 oxideqt-codecs-extra unshield vlc vlc-plugin-notify"
-
+#codecs="chromium-codecs-ffmpeg-extra gstreamer1.0-libav gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libavcodec-extra libdvdcss2 libdvdnav4 libdvdread4 oxideqt-codecs-extra unshield vlc vlc-plugin-notify"
+codecs="mint-meta-codecs"
 desktop="redshift-gtk geoclue-2.0 clipit autokey-gtk"
 
-graphics="blender shutter cheese pinta"
+graphics="gimp blender shutter cheese pinta"
 
-office="zim anki cherrytree okular"
+office="libreoffice zim anki cherrytree okular"
 
 virtualization="vagrant virtualbox-qt virtualbox-guest-additions-iso linux-image-generic linux-image-extra-virtual"
 
-browsers="chromium-browser"
+web="chromium-browser hexchat telegram gpodder"
 
-devtools="autoconf automake bison build-essential curl libapr1 libaprutil1 libc6-dev libltdl-dev libreadline6 libreadline6-dev libsqlite3-0 libsqlite3-dev libssl-dev libtool libxml2-dev libxslt-dev libxslt1-dev libyaml-dev ncurses-dev nodejs openssl sqlite3  zlib1g zlib1g-dev libgdbm-dev libncurses5-dev libffi-dev ruby-dev libcurl4-openssl-dev python-software-properties postgresql postgresql-contrib libpq-dev pgadmin3 libc6-dev nodejs"
+devtools="autoconf automake bison build-essential libapr1 libaprutil1 libc6-dev libltdl-dev libreadline6 libreadline6-dev libsqlite3-0 libsqlite3-dev libssl-dev libtool libxml2-dev libxslt-dev libxslt1-dev libyaml-dev ncurses-dev nodejs openssl sqlite3  zlib1g zlib1g-dev libgdbm-dev libncurses5-dev libffi-dev ruby-dev libcurl4-openssl-dev python-software-properties postgresql postgresql-contrib libpq-dev pgadmin3 libc6-dev nodejs sqlitebrowser git curl"
 
-repo_apps="tlp variety telegram neofetch tor-browser youtube-dl atom sublime-text docker heroku spotify-client numix-icon-theme-circle numix-folders arc-theme tor-browser docker-engine"
+repo_apps="tlp variety telegram neofetch tor-browser youtube-dl atom sublime-text docker heroku-toolbelt spotify-client numix-icon-theme-circle numix-folders arc-theme tor-browser docker-engine"
 
-package_list=$devtools" "$browsers" "$virtualization" "$office" "$graphics" "$desktop" "$compresion" "$clitools" "$system" "$codecs
+xapps="xreader xplayer xviewer pix xed"
+
+package_list=$devtools" "$internet" "$virtualization" "$office" "$graphics" "$desktop" "$compresion" "$clitools" "$system" "$codecs
+
+remove_list=""
 
 ############################################reusable functions
  
 download_install(){
-    echo "  ${GREEN}Downloading $1${NC}"
+    echo "-------------------------------------" >setup.log
+    log "  ${GREEN}Downloading $1${NC}"
     pkg=$1.deb
     wget -nv -O $pkg $2 >>setup.log 2>&1 
-    sudo dpkg -i $pkg >>setup.log 2>&1 && echo "  ${GREEN}Installed $1${NC}"
+    sudo dpkg -i $pkg >>setup.log 2>&1 && log "  ${GREEN}Installed $1${NC}"
     rm -f $pkg >>setup.log 2>&1 
 }
 
 add_repo(){
-    sudo add-apt-repository -y $2 >>setup.log 2>&1 && echo " ${GREEN} Repo $1 added${NC}" || echo "*** ${RED}Repo $i not installed${NC} ***"
+    echo "-------------------------------------" >setup.log
+    sudo add-apt-repository -y $2 >>setup.log 2>&1 && log " ${GREEN} Repo $1 added${NC}" || log "*** ${RED}Repo $i not installed${NC} ***"
 }
 
 mass_install(){
-	echo " ${GREEN} mass software install ${NC}"
-	sudo apt-fast -y install $1 >>setup.log 2>&1  || {
-		echo " ${RED} mass software install failed switching to indiviual installs ${NC}"
+    echo "-------------------------------------" >setup.log
+	log " ${GREEN} mass software install ${NC}"
+	sudo apt-fast -y install $1  || {
+		log " ${RED} mass software install failed switching to indiviual installs ${NC}"
     	for i in $1; do
-       		sudo apt-fast install  -y $i >>setup.log 2>&1 && echo " ${GREEN}  installed $i${NC}"  || echo "*** ${RED}$i not installed${NC} ***"
+            echo "-------------------------------------" >setup.log
+       		sudo apt-fast install  -y $i >>setup.log 2>&1 && log " ${GREEN}  installed $i${NC}"  || log "*** ${RED}$i not installed${NC} ***"
    		done
 	}
 }
+log(){
+echo $1
+echo -e "\n"$1"\n" > setup.log
+}
 ################### Blocks ##########################
 install_aptfast(){
-	echo "installing apt-fast"
+	log "installing apt-fast"
     sudo apt-get install -y aria2 git curl >>setup.log 2>&1   
     git clone https://github.com/ilikenwf/apt-fast /tmp/apt-fast >>setup.log 2>&1  
     sudo cp /tmp/apt-fast/apt-fast /usr/bin >>setup.log 2>&1  
@@ -70,7 +82,7 @@ install_aptfast(){
 }
 
 set_mirrors(){
-	echo "setting faster mirrors"
+	log "setting faster mirrors"
     MIRRORS="MIRRORS=( 'http://mirror.os6.org/ubuntu/,http://mirror.math.ucdavis.edu/ubuntu/,http://mirrors.us.kernel.org/ubuntu/, http://mirrors.xmission.com/ubuntu/' )"
     sudo sh -c "echo '$MIRRORS'  >> /etc/apt-fast.conf" >>setup.log 2>&1  
 
@@ -79,18 +91,16 @@ set_mirrors(){
 }
 
 add_repos(){
-	echo "Adding keys.."
+	log "Adding keys.."
 	#keys
-    #heroku
-    curl -SsL https://cli-assets.heroku.com/apt/release.key | sudo apt-key add - >>setup.log 2>&1 && echo  " ${GREEN} Added Heroku ${NC}"
    
     #spotify
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2C19886 >>setup.log 2>&1 && echo  " ${GREEN} Added Spotify ${NC}"
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2C19886 >>setup.log 2>&1 && log  " ${GREEN} Added Spotify ${NC}"
 
     #docker
-    sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D >>setup.log 2>&1 && echo  " ${GREEN} Added Docker ${NC}"
+    sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D >>setup.log 2>&1 && log  " ${GREEN} Added Docker ${NC}"
 
-	echo "Adding repositories.."
+	log "Adding repositories.."
 	#repos
     add_repo tlp        ppa:linrunner/tlp
     add_repo variety    ppa:peterlevi/ppa
@@ -102,8 +112,8 @@ add_repos(){
     add_repo numix      ppa:numix/ppa
     add_repo arc        ppa:noobslab/themes
     add_repo sublime    ppa:webupd8team/sublime-text-2
-    add_repo heroku     "deb https://cli-assets.heroku.com/branches/stable/apt ./"
-    add_repo spotify    "deb http://repository.spotify.com stable non-free"
+   add_repo spotify    "deb http://repository.spotify.com stable non-free"
+    add_repo xapps		ppa:embrosyn/xapps
 
     sudo apt-get update >>setup.log 2>&1
 }
@@ -111,72 +121,81 @@ add_repos(){
 
 install_software(){
 
-	echo "upgrading..."
-    sudo apt-fast -y upgrade >>setup.log 2>&1 
+	log "upgrading..."
+    sudo apt-fast -y upgrade
  
 
-	echo "now for the software..."
+	log "now for the software..."
     mass_install "$package_list"
 	mass_install "$repo_apps"
 
-	echo "installing slow downloads"
+	log "installing slow downloads"
     download_install rambox https://getrambox.herokuapp.com/download/linux_64?filetype=deb
     download_install skype https://go.skype.com/skypeforlinux-64-alpha.deb
     download_install discord "https://discordapp.com/api/download?platform=linux&format=deb"
     download_install slack https://downloads.slack-edge.com/linux_releases/slack-desktop-2.4.2-amd64.deb
     download_install gitter https://update.gitter.im/linux64/gitter_3.1.0_amd64.deb
     download_install gitkracken https://release.gitkraken.com/linux/gitkraken-amd64.deb
+
+log "  ${GREEN}installing heroku${NC}"
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz -O heroku.tar.gz >>setup.log 2>&1
+sudo tar -xvzf heroku.tar.gz -C /usr/local/lib >>setup.log 2>&1
+sudo /usr/local/lib/heroku/install >>setup.log 2>&1
 }
 
 rails_install(){
 
-	echo "installing rails..${GREEN}"
+	log "installing rails..${GREEN}"
 	echo ""
-	echo "  installing rvm"
-    gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3  
-    curl -sSL https://get.rvm.io | bash -s stable  
-    source ~/.rvm/scripts/rvm 
-    echo "source $HOME/.rvm/scripts/rvm" >> ~/.bash_profile
+	log "  installing rvm"
+    gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3   >>setup.log 2>&1
+    curl -sSL https://get.rvm.io | bash -s stable   >>setup.log 2>&1
+    source ~/.rvm/scripts/rvm  >>setup.log 2>&1
+    echo "source $HOME/.rvm/scripts/rvm" >> ~/.bash_profile  >>setup.log 2>&1
 
-    echo "   installing Ruby"
-    rvm install 2.4.0  
-    rvm use 2.4.0 --default  
+    log "   installing Ruby"
+    rvm install 2.4.0   >>setup.log 2>&1
+    rvm use 2.4.0 --default   >>setup.log 2>&1
     
     
-    echo "gem: --no-ri --no-rdoc" > ~/.gemrc
-    echo "   installing rails"
-    gem install rails 
+    echo "gem: --no-ri --no-rdoc" > ~/.gemrc  >>setup.log 2>&1
+    log "   installing rails"
+    gem install rails  >>setup.log 2>&1
 
-    echo "   install bundler"
-    gem install bundler 
+    log "   installing bundler"
+    gem install bundler  >>setup.log 2>&1
 
-    rvm --version
-    ruby -v
+    
+
+    rvm --version  
+    ruby -v 
     rails -v
     bundler version
+    heroku --version
 }
 
 settings(){
+log "  ${GREEN}turn on firewall${NC}"
+	log "settings.."
 
-	echo "settings.."
-
-	echo "  ${GREEN}turn on firewall${NC}"
+	
 	sudo ufw enable >>setup.log 2>&1 
 
-	echo "  ${GREEN}add user to vbox group ${NC}"
+	log "  ${GREEN}add user to vbox group ${NC}"
 	sudo adduser $USER vboxusers >>setup.log 2>&1 
 }
 
 cleanup(){
-	echo "cleaning up.." 
-	echo "  ${GREEN}clear caches${NC}"
+	log "cleaning up.." 
+	sudo apt-get remove $remove_list >>setup.log 2>&1
 	sudo apt-get clean >>setup.log 2>&1 
 	sudo apt-get -y autoremove >>setup.log 2>&1 
 }
 
+
 main(){
 	clear
-	echo "Jennifer's setup"
+	log "Jennifer's setup"
 	install_aptfast
 	set_mirrors
 	add_repos
